76163157c827d768bc1a3f049b65f8534acc6a2e
Ridavio7 <ivankov.335@mail.ru>
2023-03-29 18:43:33 +0300

    registration/errors

--- pet-project/main.go
+++ pet-project/main.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -12,12 +14,16 @@ import (
 
 var posts = []Article{}
 
-/*в объект помещаем статью, которую потом передаем в шаблон*/
-var showPost = Article{}
+var showPost = Article{} /*в объект помещаем статью, которую потом передаем в шаблон*/
 
 type Article struct {
 	Id                     uint16
 	Title, Anons, FullText string
+}
+
+type User struct {
+	Id              uint16
+	Login, Password string
 }
 
 func main() {
@@ -25,20 +31,32 @@ func main() {
 }
 
 func handleFunc() {
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	/*отслеживание через gorilla mux*/
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/", demo_web).Methods("GET")
+	rtr.HandleFunc("/main", index).Methods("GET")
 	rtr.HandleFunc("/create", create).Methods("GET") /*форма добавления новых пользователей*/
 	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/save_user", save_user).Methods("POST")
 	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET") /*отслеживанеи уникального идентификатора*/
+	rtr.HandleFunc("/authorization", authorization).Methods("GET")
+	rtr.HandleFunc("/registration", registration).Methods("GET")
+	rtr.HandleFunc("/error_input_text", error_input_text).Methods("GET")
+	rtr.HandleFunc("/error_input_reg", error_input_reg).Methods("GET")
 
 	/*обработка всех URL адресов через объект rtr*/
 	http.Handle("/", rtr)
 
-	/*обращение к css файлу*/
+	/*получаем доступ к статическим файлам*/
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
-	http.ListenAndServe(":8080", nil)
+	infoLog.Println("Запуск веб-сервера на http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	errorLog.Fatal(err)
 }
 
 func show_post(w http.ResponseWriter, r *http.Request) {
@@ -70,6 +88,7 @@ func show_post(w http.ResponseWriter, r *http.Request) {
 	for res.Next() {
 		var post Article
 		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+
 		if err != nil {
 			panic(err)
 		}
@@ -80,6 +99,7 @@ func show_post(w http.ResponseWriter, r *http.Request) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+
 	t, err := template.ParseFiles("templates/index.html",
 		"templates/header.html", "templates/footer.html")
 
@@ -88,6 +108,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+
 	if err != nil {
 		panic(err)
 	}
@@ -95,6 +116,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	res, err := db.Query("SELECT * FROM `articles`")
+
 	if err != nil {
 		panic(err)
 	}
@@ -106,9 +128,11 @@ func index(w http.ResponseWriter, r *http.Request) {
 	for res.Next() {
 		var post Article
 		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+
 		if err != nil {
 			panic(err)
 		}
+
 		/*элемент вывода данных на главную страницу*/
 		posts = append(posts, post)
 	}
@@ -123,17 +147,41 @@ func create(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
+
 	t.ExecuteTemplate(w, "create", nil)
 }
 
-/*добавления новых данных в MySQL*/
+func error_input_text(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("templates/error_input_text.html",
+		"templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "error_input_text", nil)
+}
+
+func error_input_reg(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("templates/error_input_reg.html",
+		"templates/header_for_demo.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "error_input_reg", nil)
+}
+
 func save_article(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
 
 	if title == "" || anons == "" || full_text == "" {
-		fmt.Fprintf(w, "Заполните все данные")
+		http.Redirect(w, r, "/error_input_text", http.StatusSeeOther)
 	} else {
 		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
 		if err != nil {
@@ -147,6 +195,93 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 		}
 		defer insert.Close()
 
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		http.Redirect(w, r, "/main", http.StatusSeeOther)
 	}
+}
+
+func save_user(w http.ResponseWriter, r *http.Request) {
+	login := r.FormValue("login")
+	password := r.FormValue("password")
+
+	if login == "" || password == "" {
+		http.Redirect(w, r, "/error_input_reg", http.StatusSeeOther)
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `user` (`login`, `password`) VALUES('%s', '%s')", login, password))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/main", http.StatusSeeOther)
+	}
+}
+
+func demo_web(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("templates/demo_web.html",
+		"templates/header_for_demo.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	res, err := db.Query("SELECT * FROM `articles`")
+
+	if err != nil {
+		panic(err)
+	}
+
+	/*обнуление списка, чтобы не пучковался каждый раз при обновлении*/
+	posts = []Article{}
+
+	/*Цикл перебора данных в таблице*/
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+
+		if err != nil {
+			panic(err)
+		}
+
+		/*элемент вывода данных на главную страницу*/
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "demo_web", posts)
+}
+
+func authorization(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/authorization.html",
+		"templates/header_for_demo.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "authorization", nil)
+}
+
+func registration(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/registration.html",
+		"templates/header_for_demo.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "registration", nil)
 }
